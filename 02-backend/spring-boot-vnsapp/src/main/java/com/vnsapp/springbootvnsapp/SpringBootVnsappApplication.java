00f586886e405257a1c28178d3034db645e92480
Joe Wilson <wilsonmangunsong@gmail.com>
2023-03-05 01:02:33 +0700

    create spring boot starter project

---
+++ 02-backend/spring-boot-vnsapp/src/main/java/com/vnsapp/springbootvnsapp/SpringBootVnsappApplication.java
@@ -0,0 +1,13 @@
+package com.vnsapp.springbootvnsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVnsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVnsappApplication.class, args);
+	}
+
+}
---
+++ 02-backend/spring-boot-vnsapp/src/test/java/com/vnsapp/springbootvnsapp/SpringBootVnsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vnsapp.springbootvnsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVnsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
